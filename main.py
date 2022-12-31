22ff19d89fb361c4a9979b576e3abc69a11c2385
ravenclawgt <jorge.totena@gmail.com>
2022-12-31 12:08:33 -0500

    Converting the program to functions

--- main.py
+++ main.py
@@ -1,56 +1,63 @@
-print("****Bienvenido a su calculadora virtual*****")
-numero1 = int(input("Ingrese un número "))
-print("*" * 6, "Escoja el tipo de operación",
-"*" * 6)
-counter = 0
-new_number = 0 + numero1
-while counter <= 10:
-  operacion = int(input("""Ingresa tu opción:
-  1. Suma
-  2. Resta
-  3. Multiplicación
-  4. Division
-  5. Salir
-  Ingresa aqui => """))  
-  if operacion == 1:
-    new_number = new_number + int(input("Ingrese otro numero "))
-    print("El resultado de su operación es ", new_number)
-    validation = input("Quieres continuar? (y/n) ")
-    if validation == "y":
-      continue
-    else:
-      break    
-    counter += 1
-  elif operacion == 2:
-    new_number = new_number - int(input("Ingrese otro numero "))
-    print("El resultado de su operación es ", new_number)
-    validation = input("Quieres continuar? (y/n) ")
-    if validation == "y":
+def number():
+  print("****Bienvenido a su calculadora virtual*****")
+  numero1 = int(input("Ingrese un número "))
+  print("*" * 6, "Escoja el tipo de operación",
+  "*" * 6)
+  return numero1
+
+first_number = number()
+print(first_number)
+
+def operation():  
+  counter = 0
+  new_number = 0 + first_number 
+  while counter <= 10:
+    operacion = int(input("""Ingresa tu opción:
+    1. Suma
+    2. Resta
+    3. Multiplicación
+    4. Division
+    5. Salir
+    Ingresa aqui => """))  
+    if operacion == 1:
+      new_number = new_number + int(input("Ingrese otro numero "))
+      print("El resultado de su operación es ", new_number)
+      validation = input("Quieres continuar? (y/n) ")
+      if validation == "y":
         continue
+      else:
+        break    
+      counter += 1
+    elif operacion == 2:
+      new_number = new_number - int(input("Ingrese otro numero "))
+      print("El resultado de su operación es ", new_number)
+      validation = input("Quieres continuar? (y/n) ")
+      if validation == "y":
+          continue
+      else:
+        break  
+      counter += 1
+    elif operacion == 3:
+      new_number = new_number * int(input("Ingrese otro numero "))
+      print("El resultado de su operación es ", new_number)
+      validation = input("Quieres continuar? (y/n)")
+      if validation == "y":
+          continue
+      else:
+        break 
+      counter += 1
+    elif operacion == 4:
+      new_number = new_number / int(input("Ingrese otro numero "))
+      print("El resultado de su operación es ", new_number)
+      validation = input("Quieres continuar? (y/n) ")
+      if validation == "y":
+          continue
+      else:
+        break 
+      counter += 1
+    elif operacion == 5:
+      break
     else:
-      break  
-    counter += 1
-  elif operacion == 3:
-    new_number = new_number * int(input("Ingrese otro numero "))
-    print("El resultado de su operación es ", new_number)
-    validation = input("Quieres continuar? (y/n)")
-    if validation == "y":
-        continue
-    else:
-      break 
-    counter += 1
-  elif operacion == 4:
-    new_number = new_number / int(input("Ingrese otro numero "))
-    print("El resultado de su operación es ", new_number)
-    validation = input("Quieres continuar? (y/n) ")
-    if validation == "y":
-        continue
-    else:
-      break 
-    counter += 1
-  elif operacion == 5:
-    break
-  else:
-    print("Seleccione una opción correcta, recuerde que debe ingresar el número de su operación")
-  
-  
+      print("Seleccione una opción correcta, recuerde que debe ingresar el número de su operación")
+    
+operation()
